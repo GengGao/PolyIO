1c50d1e5399ce4d83360ca1ac92df188bf9de576
Geng Gao <hello@genggao.net>
2015-05-23 15:17:38 -0700

    minor cleanup

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 var app = document.querySelector('#app');
 
-app.config ={
+app.config = {
   name: 'Google I/O 2015',
   date: 'May 28 - 29, 2015',
   location: 'San Francisco, CA',
   menu: {
-    selected: "explore"
+    selected: 'explore'
   },
   schedule: {
     selected: 0
   }
 };
 
-app.addEventListener('template-bound',function(e) {
+app.addEventListener('template-bound', function(e) {
   var draw = document.querySelector('core-drawer-panel');
   var menu = document.querySelector('core-menu');
 
   var scheduleTabs = document.querySelector('div[data-page="schedule"] paper-tabs');
   var schedulePages = document.querySelector('div[data-page="schedule"] core-pages');
-  
-  menu.addEventListener('core-select',function(e) {
+
+  menu.addEventListener('core-select', function(e) {
     draw.closeDrawer();
   });
 });
